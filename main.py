e726a62d271e5c0dc11382faca5f7ebd9de6bc0f
namlok1118 <71591955+namlok1118@users.noreply.github.com>
2025-08-05 04:27:54 +0000

    minor fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.session_state.first_stage_response = "情況的總結"
 
 example_prompt = read_file_as_string("example_prompt.txt")
 
-st.write("最近有什麼煩惱?")
+st.write("您可以把煩惱寫在下方, 並附帶一條問題, 然後根據我們的指示修改, 如果問題是合適的, 便可到下一個步驟.")
 prompt = st.text_area('', key='prompt', value = example_prompt, height=200)
 ask_button = st.button('確定')
 
